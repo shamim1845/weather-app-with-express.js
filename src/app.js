ec10496ee6c4ee3af418916ea2678384fde47a7e
shamim1845 <samimraj1845@gmail.com>
2023-11-05 23:33:06 +0600

    successfully run on localhost

--- src/app.js
+++ src/app.js
@@ -4,40 +4,33 @@ const port = process.env.PORT || 7000;
 const path = require("path");
 const hbs = require("hbs");
 
-
 const staticPath = path.join(__dirname, "../templates/assets");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
-hbs.registerPartials(partialsPath)
+hbs.registerPartials(partialsPath);
 
 //Template engine
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
 app.use(express.static(staticPath));
 
-
 //routing
 app.get("/", (req, res) => {
-    res.render("index");
-})
+  res.render("index");
+});
 app.get("/about", (req, res) => {
-    res.render("about");
-})
+  res.render("about");
+});
 app.get("/weather", (req, res) => {
-    res.render("weather");
-})
+  res.render("weather");
+});
 app.get("/*", (req, res) => {
-    res.render("404", {
-        errMsg: "Oops page not found"
-    });
-})
-
-
-
-
-
+  res.render("404", {
+    errMsg: "Oops page not found",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`port ready ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
